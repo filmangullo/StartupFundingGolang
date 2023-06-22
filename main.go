6c145127b632bd0c60d5c135f5d9a1537feaab95
filmangullo <filmantapgullo@gmail.com>
2023-06-22 23:48:48 +0700

    make generate token

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"startupfunding/auth"
 	"startupfunding/handler"
 	"startupfunding/user"
 
@@ -33,6 +35,9 @@ func main() {
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
+	authService := auth.NewService()
+
+	fmt.Println(authService.GenerateToken(1001))
 
 	userHandler := handler.NewUserHandler(userService)
 
