2e5edf15ad7dfe2e09c76935e4f3ab107c2f126c
filmangullo <filmantapgullo@gmail.com>
2023-07-04 16:50:24 +0700

    make repository campaing

--- campaign/repository.go
+++ campaign/repository.go
@@ -1,1 +1,34 @@
 package campaign
+
+type Repository struct {
+    FindAll() ([]Campaign, error)
+    FindByUserID(userID int) ([]Campaign, error)
+}
+
+type repository struct {
+    db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+    return &repository{db}
+}
+
+func FindAll(r *repository) ([]Campaign, error) {
+    var campaigns []Campaign
+    err := r.db.find(&campaigns).Error
+    if err != nil {
+        return campaigns, err
+    }
+
+    return campaigns, nil
+}
+
+func (r *repository) FindByUserID(userID int) ([]Campaign, error) {
+    var campaigns []Campaign
+    err:= r.db.Where("user_id=?", userID).Find(&campaigns).Error
+
+    if err != nil {
+        return campaigns, err
+    }
+    return campaigns, nil
+}
